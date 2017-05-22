c8ca079570c83db11ab182d485df6275605e2a5d
phayes <patrick.d.hayes@gmail.com>
2017-05-22 09:30:49 -0700

    Adding symlink code

--- freebird/SiteScriptHandler.php
+++ freebird/SiteScriptHandler.php
@@ -19,7 +19,33 @@ class SiteScriptHandler {
    * Link freebird modules, themes, and profiles
    */
   public static function createSiteSymlinks(Event $event) {
-    
+    $fs = new Filesystem();
+    $drupalFinder = new DrupalFinder();
+    $drupalFinder->locateRoot(getcwd());
+    $drupalRoot = $drupalFinder->getDrupalRoot();
+    $dirs = [
+      'modules' => 'modules',
+      'profiles' => 'profiles',
+      'themes' => 'themes',
+      'vendor/highwire/freebird/modules' => 'modules',
+      'vendor/highwire/freebird/profiles' => 'profiles',
+      'vendor/highwire/freebird/themes' => 'themes',
+    ];
+    foreach ($dirs as $dir => $type) {
+      if ($fs->exists($dir)) {
+        $contents = scandir($dir);
+        foreach ($contents as $content) {
+          if ($content != '.' && $content != '..') {
+            $link = $drupalRoot . '/' . $dir . '/' . $content;
+            $target = $fs->makePathRelative('web/' . $type . '/' . $content);
+            if (!$fs->exists($link)) {
+              $fs->symlink($target, $link, true);
+              $event->getIO()->write("Create a symlink from $link to $target");
+            }
+          }
+        }
+      }
+    }
   }
 
 }
